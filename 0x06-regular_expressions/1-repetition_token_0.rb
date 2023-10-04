4731796efa2a7ee0a8ccfe57f274eb11ef81e47a
Kaluogba <kaluogba1990@gmail.com>
2023-10-04 08:16:46 -0700

    regular expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,16 +1,2 @@
-def match_string(input)
-  regex = /hb[t]*n/
-  input.match(regex)
-end
-
-# Accept one argument from the command line
-input = ARGV[0]
-
-# Call the match_string method and print the result
-result = match_string(input)
-
-if result
-  puts "Matched: #{result[0]}"
-else
-  puts "No match found."
-end
+#!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt{2,5}n/).join
