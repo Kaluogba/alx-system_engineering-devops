4f9d741012fedcc70de3c436f6501ea5a19b71c0
Kaluogba <kaluogba1990@gmail.com>
2023-10-04 08:14:14 -0700

    regular expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,16 @@
-pattern1 = /hbn[a-z]{4}/
-pattern2 = /hbn[A-Z]{4}/
-pattern3 = /hbn\d{4}/
+def match_string(input)
+  regex = /hb[t]*n/
+  input.match(regex)
+end
 
+# Accept one argument from the command line
+input = ARGV[0]
+
+# Call the match_string method and print the result
+result = match_string(input)
+
+if result
+  puts "Matched: #{result[0]}"
+else
+  puts "No match found."
+end
